aaf6037a472f7f92f826027de04b577e2827198e
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-05 22:19:32 +0000

    Sat, Sep 5, 2026, 6:19 PM -04:00

---
+++ players/4.sql
@@ -0,0 +1,3 @@
+SELECT "first_name", "last_name" FROM "players"
+WHERE "birth_city" IS NOT 'united_states'
+ORDER BY "first_name" AND "last_name" ASC;
---
+++ players/5.sql
@@ -0,0 +1,3 @@
+SELECT "first_name", "last_name" FROM "players"
+WHERE "throws" = 'R'
+ORDER BY "first_name" AND "last_name";
