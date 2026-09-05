168fd88a81fd11358ae2229e8c950eeb5d8d5ae0
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-05 22:30:31 +0000

    Sat, Sep 5, 2026, 6:30 PM -04:00

--- players/8.sql
+++ players/8.sql
@@ -1,1 +1,2 @@
-SELECT ROUND(AVG("height"), 2) AS 'Average_Height', 
+SELECT ROUND(AVG("height"), 2) AS 'Average_Height', ROUND(AVG("weight"), 2) AS 'Average_Weight' FROM "players"
+WHERE "debut" >= '2000-01-01';
