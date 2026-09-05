57d86b69f9db99278f60dd40a6a78b9dd2900989
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-05 22:36:44 +0000

    Sat, Sep 5, 2026, 6:36 PM -04:00

--- players/10.sql
+++ players/10.sql
@@ -1,3 +1,3 @@
-SELECT ("first_name", "last_name") AS "Players_from_NY", "cirth_city" FROM "players"
+SELECT CONCAT("first_name", "last_name") AS "Players_from_NY", "birth_city" FROM "players"
 WHERE "birth_state" = 'NY'
 ORDER BY "birth_city" ASC;
