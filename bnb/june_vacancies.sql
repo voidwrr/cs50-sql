e392c53921e542505fdb6a3d5e1c5fd0cdc43b51
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-17 03:16:35 +0000

    Wed, Sep 16, 2026, 11:16 PM -04:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -1,2 +1,6 @@
 CREATE VIEW "june_vacancies" AS
-SELECT "listings"."id", "property_type", "host_name"
+SELECT "listings"."id", "property_type", "host_name", COUNT("date") AS "days_vacant"
+FROM "listings"
+JOIN "availabilities" ON "listings"."id" = "availabilities"."listing_id"
+GROUP BY "listing_id"
+HAVING "date" LIKE '2023-06-%';
