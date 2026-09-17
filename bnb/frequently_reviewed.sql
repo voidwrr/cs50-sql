e65e010cf03e454d3834703aa6b08f26bf1fe02e
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-17 03:10:47 +0000

    Wed, Sep 16, 2026, 11:10 PM -04:00

--- bnb/frequently_reviewed.sql
+++ bnb/frequently_reviewed.sql
@@ -1,2 +1,7 @@
 CREATE VIEW "frequently_reviewed" AS
-
+SELECT "id", "property_type", "host_name", COUNT("comments") AS "reviews"
+FROM "listings"
+JOIN "reviews" ON "listings"."id" = "reviews"."listing_id"
+GROUP BY "listing_id"
+ORDER BY "reviews" DESC, "host_name" ASC
+LIMIT 100;
