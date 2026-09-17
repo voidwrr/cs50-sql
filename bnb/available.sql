6c9eb11342f0ef9f43e2bb951e7ed8c16808f3ab
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-17 03:19:04 +0000

    Wed, Sep 16, 2026, 11:19 PM -04:00

--- bnb/available.sql
+++ bnb/available.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "available" AS
+CREATE VIEW "available" AS
 SELECT "listings"."id", "property_type", "host_name", "date" FROM "listings"
 JOIN "availabilities" ON "listings"."id" = "availabilities"."listing_id"
 WHERE "available" = "TRUE";
