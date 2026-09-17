68648170202f6ffded4a81404e741195aec6c4c1
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-17 02:32:04 +0000

    Wed, Sep 16, 2026, 10:32 PM -04:00

--- private/private.sql
+++ private/private.sql
@@ -3,23 +3,21 @@ CREATE TABLE "temp" (
 );
 
 INSERT INTO "temp" ("phrase")
-SELECT
-(SELECT SUBSTR("sentence", 98, 4) FROM "sentences"
-WHERE "id" = 14),
-(SELECT SUBSTR("sentence", 3, 5) FROM "sentences"
-WHERE "id" = 114),
-(SELECT SUBSTR("sentence", 72, 9) FROM "sentences"
-WHERE "id" = 618),
-(SELECT SUBSTR("sentence", 7, 3) FROM "sentences"
-WHERE "id" = 630),
-(SELECT SUBSTR("sentence", 12, 5) FROM "sentences"
-WHERE "id" = 932),
-(SELECT SUBSTR("sentence", 50, 7) FROM "sentences"
-WHERE "id" = 2230),
-(SELECT SUBSTR("sentence", 44, 10) FROM "sentences"
-WHERE "id" = 2346),
-(SELECT SUBSTR("sentence", 14, 5) FROM "sentences"
-WHERE "id" = 3041);
+SELECT SUBSTR("sentence", 98, 4) FROM "sentences" WHERE "id" = 14
+UNION ALL
+SELECT SUBSTR("sentence", 3, 5) FROM "sentences" WHERE "id" = 114
+UNION ALL
+SELECT SUBSTR("sentence", 72, 9) FROM "sentences" WHERE "id" = 618
+UNION ALL
+SELECT SUBSTR("sentence", 7, 3) FROM "sentences" WHERE "id" = 630
+UNION ALL
+SELECT SUBSTR("sentence", 12, 5) FROM "sentences" WHERE "id" = 932
+UNION ALL
+SELECT SUBSTR("sentence", 50, 7) FROM "sentences" WHERE "id" = 2230
+UNION ALL
+SELECT SUBSTR("sentence", 44, 10) FROM "sentences" WHERE "id" = 2346
+UNION ALL
+SELECT SUBSTR("sentence", 14, 5) FROM "sentences" WHERE "id" = 3041;
 
 CREATE VIEW "message" AS
 SELECT "phrase" FROM "temp";
