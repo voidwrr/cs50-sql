5f30c9fcaec55e65d1c00ea007fa2fcf4a413092
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-22 16:30:41 +0000

    Tue, Sep 22, 2026, 12:30 PM -04:00

--- dont-panic-python/hack.py
+++ dont-panic-python/hack.py
@@ -1,10 +1,12 @@
 from cs50 import SQL
 
 db = SQL("sqlite:///dont-panic.db")
+password = input("Enter a password: ")
 db.execute(
     """
     UPDATE "users"
-    SET "password" = 'hacked!'
+    SET "password" = ?
     WHERE "username" = 'admin';
-    """
+    """,
+    password
 )
