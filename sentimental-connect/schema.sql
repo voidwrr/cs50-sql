6bc983ece0ba2c1ede640c3511ab8e126326c3c9
João Salvador <149970310+voidwrr@users.noreply.github.com>
2026-09-22 21:46:19 +0000

    Tue, Sep 22, 2026, 5:46 PM -04:00

--- sentimental-connect/schema.sql
+++ sentimental-connect/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `linkedin`;
 
 USE `linkedin`
 
-CREATE TABLE `people` (
+CREATE TABLE `users` (
     `id` INT AUTO_INCREMENT,
     `first_name` VARCHAR(32) NOT NULL,
     `last_name` VARCHAR(32) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE `schools` (
     `name` VARCHAR(64) NOT NULL,
     `type` ENUM('Primary', 'Secondary', 'Higher Education') NOT NULL,
     `location` VARCHAR(64),
-    `year` DATETIME,
+    `year` YEAR,
     PRIMARY KEY(`id`)
 );
 
@@ -29,14 +29,31 @@ CREATE TABLE `companies` (
 );
 
 CREATE TABLE `connections_people` (
-    `username`
-    `friend_id`
+    `user_id` INT,
+    `friend_id` INT,
+    PRIMARY KEY(`userid`, `friend_id`),
+    FOREIGN KEY(`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY(`friend_id`) REFERENCES `users`(`id`)
 );
 
 CREATE TABLE `connections_schools` (
-
+    `user_id` INT,
+    `school_id` INT,
+    `start_date` DATE NOT NULL,
+    `end_date` DATE,
+    `degree` VARCHAR(64),
+    PRIMARY KEY(`user_id`, `school_id`, `start_date`),
+    FOREIGN KEY(`person_id`) REFERENCES `people`(`id`),
+    FOREIGN KEY(`school_id`) REFERENCES `schools`(`id`)
 );
 
 CREATE TABLE `connections_companies` (
-
+    `user_id` INT,
+    `company_id` INT,
+    `start_date` DATE NOT NULL,
+    `end_date` DATE,
+    `title` VARCHAR(64) NOT NULL,
+    PRIMARY KEY(`user_id`, `company_id`, `start_date`),
+    FOREIGN KEY(`user_id`) REFERENCES `people`(`id`),
+    FOREIGN KEY(`company_id`) REFERENCES `companies`(`id`)
 );
